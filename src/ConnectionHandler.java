8842dd6c41ea2018fe40a1e6ef5a4686618350c5
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-15 23:20:49 +0300

    Initial commit

---
+++ src/ConnectionHandler.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ConnectionHandler {
+
+    private static final String HTTP_HEADERS =
+            """
+            HTTP/1.1 200OK
+            Date: Mon, 18 Sep 2023 14:08:55 +0200
+            HttpServer: Simple Webserver
+            Content-Length: 8
+            Content-Type: text/plain
+            Content-Disposition: attachment; filename="content.txt"
+            """;
+
+    private static final String JSON = "{k: 'v'}";
+
+    private final Socket socket;
+
+    public ConnectionHandler(Socket socket) {
+        this.socket = socket;
+        handle();
+    }
+
+    public void handle() {
+        try {
+            var inputStreamReader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), StandardCharsets.US_ASCII));
+
+            var outputStreamWriter = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.US_ASCII));
+
+            parseRequest(inputStreamReader);
+            writeResponse(outputStreamWriter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void parseRequest(BufferedReader inputStreamReader) throws IOException {
+        var request = inputStreamReader.readLine();
+
+        while (request != null && !request.isEmpty()) {
+            System.out.println(request);
+            request = inputStreamReader.readLine();
+        }
+    }
+
+    private void writeResponse(BufferedWriter outputStreamWriter) {
+        try {
+            outputStreamWriter.write(HTTP_HEADERS);
+            outputStreamWriter.newLine();
+            outputStreamWriter.write(JSON);
+            outputStreamWriter.newLine();
+            outputStreamWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
